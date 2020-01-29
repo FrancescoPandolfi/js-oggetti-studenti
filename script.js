80c41b8be3eae4bfb7805b7a5d7662571d02d819
Francesco Pandolfi <francesco.pan@gmail.com>
2020-01-29 15:15:16 +0100

    Secondo parte esercizio

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ $(document).ready(function () {
 // Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
 // Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 
+console.log(`PRIMO STEP`);
+
   const studente = {
     'Nome' : 'Giovanna',
     'Cognome' : 'D’arco',
@@ -15,6 +17,36 @@ $(document).ready(function () {
 for (let key in studente) {
   console.log(`${key} : ${studente[key]}`);
 }
+
+console.log(` \nSECONDO STEP`);
+
+
+  const arrStudenti = [
+    {
+      'Nome' : 'Giovanna',
+      'Cognome' : 'D’arco',
+      'eta' : 608
+    },
+    {
+      'Nome' : 'Clark',
+      'Cognome' : 'Kent',
+      'eta' : 1200
+    },
+    {
+      'Nome' : 'Peter',
+      'Cognome' : 'Parker',
+      'eta' : 22
+    }
+  ];
+
+arrStudenti.forEach(object => {
+  console.log(`${object.Nome} ${object.Cognome}`)
+});
+
+
+// arrStudenti.forEach(function(object) {
+//   console.log(object)
+// });
 
 
 
